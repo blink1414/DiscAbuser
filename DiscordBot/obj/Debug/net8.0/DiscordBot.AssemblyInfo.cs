edb0ffed2c1d2bbd2a76ffee35e68e4cb669912a
Cristiano <cfigueiredo@whitestar.pt>
2024-11-14 10:09:04 +0000

    test

--- DiscordBot/obj/Debug/net8.0/DiscordBot.AssemblyInfo.cs
+++ DiscordBot/obj/Debug/net8.0/DiscordBot.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7784eeaf814f1a2431a0d031ff6abc0085555dfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
